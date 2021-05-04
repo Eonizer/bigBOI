1c36ba656f45baad1a5e852ca5576194e1a88ef3
Eonizer <lollips911@gmail.com>
2021-05-04 23:25:04 +0300

    add var

--- bigBOI/Assets/Scripts/PlayerController.cs
+++ bigBOI/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 10f;
+    public int weight = 1;
 
     // Start is called before the first frame update
     void Start()

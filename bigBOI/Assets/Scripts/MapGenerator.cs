cad9c72ed616eaefbcd950800b3cb26968795bb1
Eonizer <lollips911@gmail.com>
2021-05-06 01:10:23 +0300

    Map gen. basic perllin try #1
    Kinda works,  but value of perlin is always the same.
    
    Co-Authored-By: BoothaLakuza <83668704+BoothaLakuza@users.noreply.github.com>

--- bigBOI/Assets/Scripts/MapGenerator.cs
+++ bigBOI/Assets/Scripts/MapGenerator.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public int width = 100;
+    public int height = 100;
+
+    public GameObject Tile;
+
+    private int[,] coord;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        coord = new int[width,height];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {   
+                //why is this perlin guy gay? always same value = 0,47.. 
+                float fValue = Mathf.PerlinNoise(x,y);
+
+                if(fValue > 0.5f){
+                    coord[x,y] = 1;        
+                    Instantiate(Tile,new Vector3(x,y,0),Quaternion.identity,transform);                
+                }
+            }
+        }
     }
 }
